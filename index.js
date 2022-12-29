8fc27fd39996e99eb7a61c46ed212972889a5ae0
Deepali Bolsekar <deepalibolsekar@gmail.com>
2022-12-29 09:29:57 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+
+
+            function myFunction1(){
+                document.getElementById("acc1").innerHTML = "0.8333333333333334";
+
+            }
+
+            function myFunction2(){
+                document.getElementById("acc2").innerHTML = "1";
+
+            }
+            function myFunction3(){
+                document.getElementById("acc3").innerHTML = "0.9166666666666666";
+
+            }
+            function myFunction4(){
+                document.getElementById("acc4").innerHTML = "0.8333333333333334";
+
+            }
+            function myFunction5(){
+                document.getElementById("acc5").innerHTML = "0.9166666666666666";
+
+            }
